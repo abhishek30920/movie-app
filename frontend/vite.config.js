a782c02f326388568b5188bc3b9b59647c9bf223
abhishek30920 <abhishek309200@gmail.com>
2025-02-07 23:26:34 +0530

    issue solved

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,26 +3,13 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
-export default defineConfig(({ mode }) => {
-  const envDir = path.resolve(__dirname, "./../frontend");
-  const env = loadEnv(mode, envDir);
+export default defineConfig(({}) => {
 
-  // Optional: Add validation for critical env variables
-  if (!env.VITE_SERVER) {
-    console.error("VITE_SERVER is not defined!");
-    throw new Error("Missing VITE_SERVER environment variable");
-  }
 
   return {
-    envDir,
+ 
     plugins: [react(), tailwindcss()],
-    server: {
-      proxy: {
-        '/api/':  env.VITE_SERVER,
-      
-       
-        
-      },
+   
     
    
-}}});
+}});
