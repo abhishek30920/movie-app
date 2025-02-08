9c093ff0160e183e3a9325e7fb58fc9b1aa4878c
abhishek30920 <abhishek309200@gmail.com>
2025-02-08 13:00:05 +0530

    cookies issue solved

--- backend/utils/createToken.js
+++ backend/utils/createToken.js
@@ -5,16 +5,16 @@ const createToken = (res,userId) => {
   const token=jwt.sign({userId},process.env.JWT_SECRET,{
     expiresIn:"30d"
   })
-
+console.log(token)
   // set jwt as an http cookie
 
-res.cookie('jwt',token,{
-    httpOnly:true,
-    secure:process.env.NODE_ENV !== 'development',
-    maxAge:30*24*60*60*1000,
-    sameSite: 'strict'
-})
-
+  res.cookie('token', token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== 'development',  // Only send over HTTPS if not in development
+    sameSite: process.env.NODE_ENV === 'development' ? 'lax' : 'none',
+    maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days
+  });
+  
 return token
 
 }
